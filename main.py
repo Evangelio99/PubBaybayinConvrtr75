611cbfcbfbf2ae9d9ce64a3bdc333a71cac96883
Evangelio99 <arnel.evangelio99@gmail.com>
2021-06-06 15:33:17 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,11 @@
+from tkinter import *
+from tkinter import filedialog
+from PIL import ImageTk, Image
+import cv2
+import pytesseract
+
+root = Tk()
+root.title('Baybayin Image Translator')
+root.geometry('900x700')
+
+root.mainloop()
