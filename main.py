362bc03ae70b20eeb3a18ef6a6bdae99e27c4c02
shlxfrbs09 <58224311+shlxfrbs09@users.noreply.github.com>
2021-06-08 20:04:40 +0800

    GUI: Customization

--- main.py
+++ main.py
@@ -4,23 +4,13 @@ from PIL import ImageTk, Image
 import cv2
 import pytesseract
 from tkinter import PhotoImage
-
-#import tkinter as tk
-#import PyPDF2
-#from PIL import Image, ImageTk
 from tkinter.filedialog import askopenfile
 
-# dictionary of colors:
-color = {"nero": "#252726", "orange": "#FF8700", "darkorange": "#FE6101"}
-
 pytesseract.pytesseract.tesseract_cmd = 'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
-root = Tk()
-root.title('Baybayin Image Translator')
-icn=PhotoImage(file='C:\\Users\\shiel\\Desktop\\python-environment\\baybayinIMGTrans\\icon.png')
-root.iconphoto(False,icn)
-root.resizable(False, False) #Para mawala yung full screen button hehe
-#root.configure(bg='blue')
 
+root = Tk()
+
+# window
 #root.geometry('900x750') #w x h
 width_of_window=900
 height_of_window=750
@@ -29,20 +19,97 @@ screen_height=root.winfo_height()
 x_coordinate=(screen_width/2)-(width_of_window/2)
 y_coordinate=(screen_height/2)-(height_of_window/2)+395
 root.geometry("%dx%d+%d+%d"%(width_of_window,height_of_window,x_coordinate,y_coordinate))
+root.title('Baybayin Image Translator')
+icn=PhotoImage(file="icon.png")
+root.iconphoto(False,icn)
+root.resizable(False, False) # para mawala yung full screen button hehe
+scrollbar = Scrollbar(root)
+scrollbar.pack( side = RIGHT, fill = Y )
+#root.configure(bg='blue')
+
+# dictionary of colors:
+color = {"nero": "#252726", "orange": "#FF8700", "darkorange": "#FE6101"}
 
 # setting switch state:
 btnState = False
 
-# loading Navbar icon image:
+# loading of images:
 navIcon = PhotoImage(file="menu.png")
 closeIcon = PhotoImage(file="close.png")
 uploadbtn1 = PhotoImage(file="uploadbtn.png")
 extractbtn1= PhotoImage(file="extractbtn.png")
+box = PhotoImage(file="box.png")
 
+# declaring new var for images for placing
 upload_label = Label(image=uploadbtn1)
 extract_label = Label(image=extractbtn1)
+box_label = Label(image=box)
+box_label.place(x=15,y=360)
+
+# instruction label
+instructions = Label(root, text="Select an Image file on your computer to extract all its text", font=('Bahnschrift',10),bg="#f0f0f0",fg="black")
+instructions.place(x=280,y=270)
+
+# logo
+logo = Image.open('logo.png')
+logo = ImageTk.PhotoImage(logo)
+logo_label = Label(image=logo)
+logo_label.image = logo
+logo_label.place(x=312,y=100)
+
+# vars
+newline= Label(root)
+uploaded_img=Label(root)
+
+# uploading of picture
+def upload():
+    try:
+        path=filedialog.askopenfilename()
+        image=Image.open(path)
+        img=ImageTk.PhotoImage(image)
+        uploaded_img.place(x=70,y=485)
+        uploaded_img.configure(image=img)
+        uploaded_img.image=img
+        show_extract_button(path)
+    except:
+        pass 
+
+# extract button
+def show_extract_button(path):
+    extractBtn= Button(root,image=extractbtn1,command=lambda: extract(path),fg="gray", borderwidth=0,font=('Times',15,'bold'))
+    extractBtn.place(x=600,y=300)
 
 
+# extraction
+def extract(path):
+    Actual_image = cv2.imread(path)
+    Sample_img = cv2.resize(Actual_image,(400,350))
+    Image_ht,Image_wd,Image_thickness = Sample_img.shape
+    Sample_img = cv2.cvtColor(Sample_img,cv2.COLOR_BGR2RGB)
+    texts = pytesseract.image_to_data(Sample_img) 
+    mytext=""
+    prevy=0
+    for cnt,text in enumerate(texts.splitlines()):
+        if cnt==0:
+            continue
+        text = text.split()
+        if len(text)==12:
+            x,y,w,h = int(text[6]),int(text[7]),int(text[8]),int(text[9])
+            if(len(mytext)==0):
+                prey=y
+            if(prevy-y>=10 or y-prevy>=10):
+                print(mytext)
+                Label(root,text=mytext,font=('Times',15,'bold')).pack()
+                #first=Label(root,text=mytext,font=('Times',15,'bold'))
+                mytext=""
+                #first.place(x=600,y=500)
+            mytext = mytext + text[11]+" "
+            prevy=y
+    Label(root,text=mytext,font=('Times',15,'bold')).pack()
+
+# upload button
+uploadbtn = Button(root,image=uploadbtn1,command=upload, borderwidth=0 ,fg="gray",font=('Times',15,'bold'))
+uploadbtn.place(x=153,y=300)
 
 # setting switch function:
 def switch():
@@ -70,32 +137,32 @@ def switch():
         #root.config(bg=color["nero"])
         instructions.config(bg="#f0f0f0",fg="#858585")
         
-
         # created animated Navbar opening:
         for x in range(-300, 0):
             navRoot.place(x=x, y=0)
             topFrame.update()
 
-        # turing button ON:
+        # turning button ON:
         btnState = True
 
-# top Navigation bar:
+
+# top navigation bar:
 topFrame = Frame(root, bg="white")
 topFrame.pack(side="top", fill=X)
 
-# Header label text:
+# header label text:
 homeLabel = Label(topFrame, text="Baybayin Converter", font="Bahnschrift 15", bg="white", fg="gray17", height=2, padx=20)
 homeLabel.pack(side="right")
 
-# Header label text:
+# header label text:
 homeLabel1 = Label(topFrame, text="        LOGO", font="Bahnschrift 15", bg="white", fg="gray17", height=2, padx=20)
 homeLabel1.pack(side="left")
 
-# Main label text:
+# main label text:
 #brandLabel = Label(root, font="System 30", bg="#f0f0f0", fg="green")
 #brandLabel.place(x=100, y=250)
 
-# Navbar button:
+# navbar button:
 navbarBtn = Button(topFrame, image=navIcon, bg="white", activebackground="#1cbdbd", bd=0, padx=20, command=switch)
 navbarBtn.place(x=10, y=10)
 
@@ -106,8 +173,10 @@ Label(navRoot, font="Bahnschrift 15", bg="white", fg="black", height=2, width=30
 
 # set y-coordinate of Navbar widgets:
 y = 80
+
 # option in the navbar:
 options = ["Profile", "Settings", "Help", "About", "Feedback"]
+
 # Navbar Option Buttons:
 for i in range(5):
     Button(navRoot, text=options[i], font="BahnschriftLight 15", bg="white", fg="#1cbdbd", activebackground="#9c9c9c", activeforeground="green", bd=0).place(x=25, y=y)
@@ -117,72 +186,12 @@ for i in range(5):
 closeBtn = Button(navRoot, image=closeIcon, bg="white", activebackground="#1cbdbd", bd=0, command=switch)
 closeBtn.place(x=250, y=10)
 
-
-#Logo
-logo = Image.open('logo.png')
-logo = ImageTk.PhotoImage(logo)
-logo_label = Label(image=logo)
-logo_label.image = logo
-logo_label.place(x=312,y=100)
-
-#instructions
-instructions = Label(root, text="Select an Image file on your computer to extract all its text", font=('Bahnschrift',10),bg="#f0f0f0",fg="black")
-instructions.place(x=280,y=270)
-
-
-newline= Label(root)
-uploaded_img=Label(root)
-scrollbar = Scrollbar(root)
-scrollbar.pack( side = RIGHT, fill = Y )
-
-def extract(path):
-    Actual_image = cv2.imread(path)
-    Sample_img = cv2.resize(Actual_image,(400,350))
-    Image_ht,Image_wd,Image_thickness = Sample_img.shape
-    Sample_img = cv2.cvtColor(Sample_img,cv2.COLOR_BGR2RGB)
-    texts = pytesseract.image_to_data(Sample_img) 
-    mytext=""
-    prevy=0
-    for cnt,text in enumerate(texts.splitlines()):
-        if cnt==0:
-            continue
-        text = text.split()
-        if len(text)==12:
-            x,y,w,h = int(text[6]),int(text[7]),int(text[8]),int(text[9])
-            if(len(mytext)==0):
-                prey=y
-            if(prevy-y>=10 or y-prevy>=10):
-                print(mytext)
-                Label(root,text=mytext,font=('Times',15,'bold')).pack()
-                mytext=""
-            mytext = mytext + text[11]+" "
-            prevy=y
-    Label(root,text=mytext,font=('Times',15,'bold')).pack()
-
-def show_extract_button(path):
-    extractBtn= Button(root,image=extractbtn1,command=lambda: extract(path),fg="gray", borderwidth=0,font=('Times',15,'bold'))
-    extractBtn.place(x=600,y=320)
-
-def upload():
-    try:
-        path=filedialog.askopenfilename()
-        image=Image.open(path)
-        img=ImageTk.PhotoImage(image)
-        uploaded_img.place(x=100,y=400)
-        uploaded_img.configure(image=img)
-        uploaded_img.image=img
-        show_extract_button(path)
-    except:
-        pass  
+# 
+#newline.configure(text='\n')
+newline.pack()
+uploaded_img.pack()
 
 #uploadbtn = Button(root,text="Upload an image",command=upload,bg="#2f2f77",fg="gray",height=2,width=20,font=('Times',15,'bold')).pack()
 
-uploadbtn = Button(root,image=uploadbtn1,command=upload, borderwidth=0 ,fg="gray",font=('Times',15,'bold'))
-uploadbtn.place(x=153,y=320)
-
-
-newline.configure(text='\n')
-newline.pack()
-uploaded_img.pack()
 
 root.mainloop()
